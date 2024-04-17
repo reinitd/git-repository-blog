8bd47963de6c42301b8f8aaaa253d60ed6865bde
QAEZZ <russian.devv@gmail.com>
2024-04-17 08:31:12 -0500

    lots

--- README.md
+++ README.md
@@ -5,15 +5,48 @@
 Create a `blogconfig.json` file in the site's root directory:
 ```json
 {
-"repository": {
-    "link": "https://github.com/octocat/example",
+  "repository": {
+    "host": "github",
+    "repository": "octocat/example",
     "branch": "main"
   },
   "paths": {
-    "posts_json": "data/posts.json",
-    "comments_json": "data/comments.json",
-    "likes_json": "data/likes.json"
+    "posts_json": "data/blog.json"
   }
+}
+```
+
+Create a 'posts.json' file anywhere in a publicly accessible place in the project directory. You can leave this blank, here is an example of what it will look like:
+```json
+{
+  "last_updated_timestamp": 1713358687,
+  "posts": [
+    {
+      "id": 0,
+      "uid": 4321,
+      "headline": false,
+      "title": "Blog Post Title",
+      "title_link": "https://github.com",
+      "author": "John Doe",
+      "author_avatar": "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+      "content": "Blog post content here.",
+      "creation_timestamp": 1712927166,
+      "edited_timestamp": null,
+      "likes": 0,
+      "shares": 0,
+      "comments": [
+        {
+          "id": 0,
+          "uid": 1234,
+          "author": "Comment Author",
+          "author_avatar": "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+          "comment_content": "Comment content here.",
+          "comment_timestamp": 1713273446,
+          "likes": 0
+        }
+      ]
+    }
+  ]
 }
 ```
 
@@ -24,3 +57,4 @@ Add the JS:
 
 ### To-do
 - [ ] Add support for GitLab and (maybe) Bitbucket.
+- [ ] Add support for GitLab instances (subdomains).
--- src/gitRepoBlog.js
+++ src/gitRepoBlog.js
@@ -4,14 +4,11 @@
 //
 //
 
-var config;
-var posts_url;
-var comments_url;
-var likes_url;
 
-var blog_posts;
-var blog_comments;
-var blog_likes;
+var config;
+var postData_url;
+
+var postData;
 
 function checkUrl(string) {
     let givenURL;
@@ -24,23 +21,114 @@ function checkUrl(string) {
     return true;
 }
 
+async function getData(url) {
+    const response = await fetch(url);
+    if (response.status != 200) {
+        resp = response.statusText;
+    } else {
+        resp = response.json();
+    }
+
+    return [response.status, resp];
+}
+
+function defaultLoadPosts(parentElementId) {
+    var start = Date.now();
+    var parentElement;
+    parentElement = document.getElementById(parentElementId);
+
+    if (parentElement === null) {
+        console.log(`Parent Element with ID ${parentElementId} does not exist!`);
+        return
+    }
+
+    postData = postData.posts;
+
+    postData.sort(function (a, b) {
+        return b.id - a.id;
+    });
+
+    parentElement.classList.add("grb-posts");
+
+    for (var i = 0; i < postData.length; i++) {
+        var post = postData[i];
+        var postDiv = document.createElement('div');
+        postDiv.classList.add('grb-post');
+
+        var titleLink = document.createElement('a');
+        titleLink.href = post.titleLink;
+        titleLink.target = "_blank";
+        titleLink.classList.add("grb-post-title");
+        titleLink.textContent = post.title;
+
+        var postContent = document.createElement('p');
+        postContent.classList.add("grb-post-content");
+        postContent.textContent = post.content.replace("<!--start-edited-tag-->", "").replace("<!--end-edited-tag-->", "");
+
+        var uidPortion = document.createElement('p');
+        uidPortion.classList.add("grb-post-uid");
+        uidPortion.textContent = `\nPost UID: ${post.uid}`;
+
+        var postStats = document.createElement('span');
+        postStats.classList.add("grb-stats");
+        postStats.appendChild(
+            document.createElement('p').textContent = post.likes
+        );
+        postStats.appendChild(
+            document.createElement('p').textContent = post.shares
+        ); // TODO: fix
+
+        postDiv.appendChild(titleLink);
+        postDiv.appendChild(uidPortion);
+        postDiv.appendChild(postContent);
+        postDiv.appendChild(postStats);
+        // TODO: make a builder
+
+        if (post.headline) {
+            postDiv.classList.add("grb-post-pinned");
+            parentElement.prepend(postDiv);
+        } else {
+            parentElement.appendChild(postDiv);
+        }
+
+        var timeTaken = Date.now() - start;
+        console.log("Posts loaded in: " + timeTaken + " milliseconds");
+
+    }
+}
 
 async function startGRB() {
     const configResponse = await fetch("./blogconfig.json");
-    config = configResponse.json();
-    let host = config.repository.link.split("/")[1].split(".");
-    switch(host.toLowerCase()) {
-        case "github.com":
-            // https://github.com/QAEZZ/DSTP/blob/main/src/main.py
-            let base = `${config.repository.link}/blob/${config.repository.branch}`;
-            posts_url = `${base}/${config.paths.posts_json}`
-            comments_url = `${base}/${config.paths.comments_json}`
-            likes_url = `${base}/${config.paths.likes_json}`
+    config = await configResponse.json();
+    console.log(config);
+    switch (config.repository.host.toString().toLowerCase()) {
+        case "github":
+            // https://raw.githubusercontent.com/The-Holy-Church-of-Terry-Davis/The-Holy-Church-of-Terry-Davis.github.io/main/js/loadposts.js
+            let base = `https://raw.githubusercontent.com/${config.repository.repository}/${config.repository.branch}`;
+            postData_url = `${base}/${config.paths.posts_json}`
             break;
+        case "gitlab":
+            console.log("GitLab is not yet supported.")
+            return;
+        case "custom":
+            console.log("Custom hosts are not yet supported.");
+            return;
         default:
             console.log(`Host '${host}' is not supported.`);
             return;
     }
-    
+    await getData(postData_url);
 }
-await startGRB()
+
+(async () => {
+    await startGRB();
+    postData = await getData(postData_url);
+    if (postData[0] != 200) {
+        console.log(`HTTP ${postData[0]} for blog.json with URL:\n${postData_url}`);
+        return;
+    }
+    postData = await postData[1];
+    console.log(postData);
+
+    defaultLoadPosts("blog-posts");
+})();
